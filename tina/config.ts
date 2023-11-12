e57c54c6e5571e842c572c62c28145f3446769c9
tenmihi <tenmihi@gmail.com>
2023-11-12 23:27:06 +0900

    feat: tinaからmdx編集して公開可能に

---
+++ tina/config.ts
@@ -0,0 +1,58 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+  branch,
+  clientId: null, // Get this from tina.io
+  token: null, // Get this from tina.io
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public",
+  },
+  media: {
+    tina: {
+      mediaRoot: "images",
+      publicFolder: "public",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "dev_post",
+        label: "Dev Posts",
+        path: "src/content/dev-posts",
+        format: 'mdx',
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "datetime",
+            name: "pubDate",
+            label: "Date Posted",
+            required: true,
+          },
+          {
+            type: "datetime",
+            name: "updatedDate",
+            label: "Date Updated",
+            required: false,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+      },
+    ],
+  },
+});
