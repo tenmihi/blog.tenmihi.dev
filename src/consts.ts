619c252c4ea65b4b5efebe6e803d052917b6930e
tenmihi <tenmihi@gmail.com>
2023-11-15 00:00:48 +0900

    chore: NotoSansへfontを載せ替え

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,4 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
 export const SITE_TITLE = 'blog.tenmihi.dev';
 export const SITE_DESCRIPTION = 'tenmihi\'s blog';
+export const GITHUB_USER_NAME = 'tenmihi'
+export const TWITTER_USER_NAME = 'tenmihi'
