ed13adffe7aa3422a2834bfd4d224d84eb93367b
tenmihi <tenmihi@gmail.com>
2024-03-24 15:07:02 +0900

    feat: add caption for img

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,13 @@ import sitemap from '@astrojs/sitemap';
 
 import tailwind from "@astrojs/tailwind";
 
+import rehypeCaptionForImg from "rehype-caption-for-img";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap(), tailwind()],
+  markdown: {
+    rehypePlugins: [() => rehypeCaptionForImg({ removeTitle: true })]
+  }
 });
